daddbc928f1e557d9946b7a88cfb1709ce8c8bfd
zyegfryed <zyegfryed@gmail.com>
2012-03-23 17:01:20 +0100

    Fixed status code chekc when doing POST request.

--- dnsimple/api.py
+++ dnsimple/api.py
@@ -57,7 +57,7 @@ class Domain(object):
             'record[prio]': prio,
         }
         response = self.dnsimple.requests.post('/domains/%s/records' % self.name, data)
-        if response.status_code == requests.codes.ok:
+        if response.status_code == requests.codes.created:
             uncache(self, 'records')
             return True
         else:
